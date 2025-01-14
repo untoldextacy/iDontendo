855fd5c2a6ea52cd0b6a392608a113878e6d3f3a
untoldextacy <vanlarec1991@gmail.com>
2025-01-14 17:39:45 -0500

    Initial Emulator Build

---
+++ main.js
@@ -0,0 +1,25 @@
+// Load the jsnes emulator
+const canvas = document.getElementById('emulatorCanvas');
+const romInput = document.getElementById('romInput');
+const nes = new jsnes.NES({
+  onFrame: function (frameBuffer) {
+    const ctx = canvas.getContext('2d');
+    const imageData = ctx.createImageData(256, 240);
+    for (let i = 0; i < frameBuffer.length; i++) {
+      imageData.data[i] = frameBuffer[i];
+    }
+    ctx.putImageData(imageData, 0, 0);
+  },
+});
+
+romInput.addEventListener('change', function (event) {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      nes.loadROM(e.target.result);
+      nes.start();
+    };
+    reader.readAsBinaryString(file);
+  }
+});
