c49e064aa2c870f82908638421327290778422fd
untoldextacy <vanlarec1991@gmail.com>
2025-01-14 23:37:07 -0500

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.', // This sets the root directory to the project root, where index.html currently is
+  base: '/iDontendo/',
   build: {
     outDir: 'dist', // Output directory for build files
   },
